6a177181b0826625244b58485fe3214bf4123253
Gunpriya Sarpate <sarpatepriya@gmail.com>
2023-04-24 21:32:47 +0530

    expanded cards

--- component/Cards.jsx
+++ component/Cards.jsx
@@ -6,18 +6,24 @@ function Cards() {
 
   return (
     <div className="card-container">
-      <motion.div className="card">
-        <motion.h2>Stranger Things</motion.h2>
-        <motion.div>
-          <p>
-            lorem ispum Stranger Things lorem ispum Stranger Thingslorem ispum
-            Stranger Things lorem ispum Stranger Things lorem ispum Stranger
-          </p>
-          <p>
-            lorem ispum Stranger Things lorem ispum Stranger Thingslorem ispum
-            Stranger Things
-          </p>
-        </motion.div>
+      <motion.div
+        transition={{ layout: { duration: 1, type: "spring" } }}
+        onClick={() => setIsOpen(!isOpen)}
+        className="card"
+      >
+        <motion.h2 layout="position">Stranger Things</motion.h2>
+        {isOpen && (
+          <motion.div className="expand">
+            <p>
+              lorem ispum Stranger Things lorem ispum Stranger Thingslorem ispum
+              Stranger Things lorem ispum Stranger Things lorem ispum Stranger
+            </p>
+            <p>
+              lorem ispum Stranger Things lorem ispum Stranger Thingslorem ispum
+              Stranger Things
+            </p>
+          </motion.div>
+        )}
       </motion.div>
     </div>
   );

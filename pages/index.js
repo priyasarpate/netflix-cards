d86bdd1f90acd7fe6942ff6a1aca2bbe20d090ad
Gunpriya Sarpate <sarpatepriya@gmail.com>
2023-04-26 16:04:34 +0530

    fteched imdb api

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Cards from "@/component/Cards";
+import API from "@/component/API";
+
 
 export default function Home() {
   return (
@@ -11,6 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Cards />
+      <API />
     </>
   );
 }
